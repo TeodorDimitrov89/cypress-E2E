56006254269c0b131ea6705bb20498cae7cf5373
Teodor Dimitrov <teodordimitrov908@gmail.com>
2024-03-07 12:12:00 +0200

    more tests

--- 02.Basic-Fundamentals/cypress/e2e/tasks.cy.js
+++ 02.Basic-Fundamentals/cypress/e2e/tasks.cy.js
@@ -11,6 +11,7 @@ describe("tasks management", () => {
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
   });
+
   it("should create a new task", () => {
     cy.visit("http://localhost:5173/");
     cy.contains("Add Task").click();
@@ -29,5 +30,39 @@ describe("tasks management", () => {
     cy.contains("Add Task").click();
     cy.get(".modal").contains("Add Task").click();
     cy.contains("Please provide values");
+  });
+
+  it("should filter tasks", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("New Task");
+    cy.get("#summary").type("Some description");
+    cy.get("#category").select("urgent");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("moderate");
+    cy.get(".task").should("have.length", 0);
+    cy.get("#filter").select("urgent");
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("all");
+    cy.get(".task").should("have.length", 1);
+  });
+
+  it("should add multiple tasks", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("Task 1");
+    cy.get("#summary").type("First Task");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+
+    cy.contains("Add Task").click();
+    cy.get("#title").type("Task 2");
+    cy.get("#summary").type("Second Task");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 2);
+
+    cy.get(".task").eq(0).contains("First Task"); // or using first
+    cy.get(".task").eq(1).contains("Second Task"); // or using last
   });
 });
