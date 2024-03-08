b4e9e5ca737787eae10c19e0c2c56d15016441e7
Teodor Dimitrov <teodordimitrov908@gmail.com>
2024-03-08 11:28:56 +0200

    working with alias

--- 03.Advanced/cypress/e2e/contact.cy.js
+++ 03.Advanced/cypress/e2e/contact.cy.js
@@ -4,13 +4,20 @@ describe("contact form", () => {
     cy.visit("http://localhost:5173/about");
     cy.get('[data-cy="contact-input-message"]').type("Hello World!");
     cy.get('[data-cy="contact-input-name"]').type("Jonh Doe");
-    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
     cy.get('[data-cy="contact-input-email"]').type("jonh@gmail.com");
-    cy.get('[data-cy="contact-btn-submit"]').should("not.be.disabled");
-    cy.get('[data-cy="contact-btn-submit"]').click();
-    cy.get('[data-cy="contact-btn-submit"]').contains("Sending...");
-    cy.get('[data-cy="contact-btn-submit"]').should("be.disabled");
 
+    cy.get('[data-cy="contact-btn-submit"]')
+      .contains("Send Message")
+      .should("not.be.disabled");
+
+    // cy.get('[data-cy="contact-btn-submit"]').should("not.be.disabled");
+
+    cy.get('[data-cy="contact-btn-submit"]').as("submitBtn"); //Alias
+    cy.get("@submitBtn").click();
+    cy.get("@submitBtn").contains("Sending...");
+    cy.get("@submitBtn").should("be.disabled");
+
+    // cy.get('[data-cy="contact-btn-submit"]').should("be.disabled");
     // cy.get('[data-cy="contact-btn-submit"]').should("have.attr", 'disabled');
   });
 });
